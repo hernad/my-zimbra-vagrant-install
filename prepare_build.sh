7782b8eaed38c91e02b050950087d15df7b9902a
Ernad Husremovic <hernad@bring.out.ba>
2017-03-17 17:31:03 +0100

    VBoxManage full path

--- prepare_build.sh
+++ prepare_build.sh
@@ -2,7 +2,7 @@
 
 
 if [ ! -f data_zimbra_dev.vdi ] ;  then
-    VBoxManage clonemedium ~/data_zimbra_dev.vdi data_zimbra_dev.vdi
+  /opt/VirtualBox/VBoxManage clonemedium ~/data_zimbra_dev.vdi data_zimbra_dev.vdi
 else
 	echo "data_zimbra_dev.vdi already exists"
 fi
