838f2351fc57962153d17de8e1b28148d7a05bba
Ernad Husremovic <hernad@bring.out.ba>
2017-03-21 08:36:13 +0100

    set hostname

--- my-zimbra_install.sh
+++ my-zimbra_install.sh
@@ -6,6 +6,28 @@ if [ x$1 == x--init ] ; then
   sudo apt-get remove -y zimbra-core # na osnovu zimbra-core installer utvrdjuje da li je nova instalacija ili upgrade
   sudo mkdir -p /opt/zimbra/install_git
   sudo chown vagrant /opt/zimbra/install_git
+
+
+  sudo apt-get install -y libnet-ldap-perl libdbi-perl silversearcher-ag
+
+  # zimbra pgp key
+  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9BE6ED79
+  sudo apt-get install -y apt-transport-https
+
+  sudo su -c "echo 'deb https://repo.zimbra.com/apt/87 xenial zimbra' >> /etc/apt/sources.list"
+  sudo apt-get update
+
+  cd /opt/zimbra/install_git
+  git clone https://github.com/hernad/my-zimbra-core-components.git
+
+  cd my-zimbra-core-components
+  cat large/zimbra-clamav-db* > zimbra-clamav-db.deb
+  cat large/zimbra-openjdk* > zimbra-openjdk.deb
+
+  sudo dpkg -i *.deb
+  sudo apt-get install -f -y
+  #sudo dpkg -i zimbra-memcached*.deb zimbra-libevent-lib*
+  #sudo apt-get install -f -y
 fi
 
 
@@ -16,40 +38,16 @@ if [ ! -d /opt/zimbra/install_git ]  ; then
 fi
 
 cd /opt/zimbra/install_git
-
-git clone https://github.com/hernad/my-zimbra-core-components.git
-
-cd my-zimbra-core-components
-
-cat large/zimbra-clamav-db* > zimbra-clamav-db.deb
-cat large/zimbra-openjdk* > zimbra-openjdk.deb
-
-sudo dpkg -i *.deb
-sudo apt-get install -f -y
-sudo dpkg -i zimbra-memcached*.deb zimbra-libevent-lib*
-sudo apt-get install -f -y
-
-sudo apt-get install -y libnet-ldap-perl libdbi-perl silversearcher-ag
-
-# zimbra pgp key
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9BE6ED79
-sudo apt-get install -y apt-transport-https
-
-sudo su -c "echo 'deb https://repo.zimbra.com/apt/87 xenial zimbra' >> /etc/apt/sources.list"
-sudo apt-get update
-
-cd /opt/zimbra/install_git
 tar xvf /vagrant/zcs.tar.gz
 cd zcs-*
 
-#export ZM_CUR_MAJOR=8  ZM_CUR_MINOR=7  ZM_CUR_MICRO=1
-
-unset ZM_CUR_MAJOR ZM_CUR_MINOR ZM_CUR_MICRO
-
 echo "FORCE NEW install"
-sudo rm /opt/zimbra/.install_history
 sudo apt-get remove -y zimbra-core
-#export ZIMBRA_FORCE_NEW_INSTALL="yes" => ignore DETECTDIRS='db bin/zmcontrol redolog index store conf/localconfig.xml data'
 
-sudo su -c "ZIMBRA_HOSTNAME=vagrant.out.ba ZIMBRA_DOMAIN=out.ba ZIMBRA_FORCE_NEW_INSTALL=yes ./install.sh"
+ZIMBRA_HOSTNAME=vagrant.out.ba
+ZIMBRA_DOMAIN=out.ba
 
+sudo su -c "echo $ZIMBRA_HOSTNAME > /etc/hostname"
+sudo su -c "hostname $ZIMBRA_HOSTNAME"
+sudo su -c "sed -i -e s/vagrant.vm/$ZIMBRA_HOSTNAME/ /etc/hosts"
+sudo su -c "ZIMBRA_HOSTNAME=$ZIMBRA_HOSTNAME ZIMBRA_DOMAIN=$ZIMBRA_DOMAIN ZIMBRA_FORCE_NEW_INSTALL=yes ./install.sh"
